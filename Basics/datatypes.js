7302fcdc720067c3be2f4d668444941b03e6f9f7
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-01-12 17:38:54 +0000

    datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // Treat all js code as newer version
+
+// alert("hii");  // we wre using node.js, not browser
+
+// console.log(5 + 2);
+
+let name = "sutun";
+let age = 23;
+let isLoggedIn = false;
+let state = null;
+let city;
+
+// Primitive datatype
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assign yet
+// symbol=> uniqueness
+
+// Non=primitive datatype
+
+// object
+
+console.log(typeof age); // number
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
