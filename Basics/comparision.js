c2b3c74996d3c28ae9557fea0319fe575a3e70ea
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-01-13 09:59:02 +0000

    comparision

---
+++ Basics/comparision.js
@@ -0,0 +1,27 @@
+// console.log(20 > 10);  //true
+// console.log(20 >= 10); //true
+// console.log(20 < 10);  //false
+// console.log(20 <= 10);  //false
+// console.log(20 == 10);  //false
+// console.log(20 != 10);  //true
+
+// console.log("2" > 1);   //true
+// console.log("02" > 1);  //true 
+
+
+// console.log(null > 0);  //false
+// console.log(null == 0);  //false
+// console.log(null >= 0);  //true
+
+// console.log(undefined == 0);  //false
+// console.log(undefined >= 0);  //false
+// console.log(undefined <= 0);  //false
+// console.log(undefined < 0);  //false
+// console.log(undefined == null);  //true
+// console.log(undefined >= null);  //false
+// console.log(undefined <= null);  //false
+
+// === => it strictly checks both values and datatypes
+
+console.log("2" === 2);  //false
+console.log("2" == 2);   //true
