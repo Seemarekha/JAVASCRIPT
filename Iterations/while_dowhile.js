863d2feec1c129ca45b851f3a1ffe8053ddfbdbd
Seemarekha <129779011+Seemarekha@users.noreply.github.com>
2025-01-24 17:39:36 +0000

    while and do-while loops

---
+++ Iterations/while_dowhile.js
@@ -0,0 +1,35 @@
+/***************** while loop **************/
+
+// let index = 0;
+// while (index <= 10) {
+//     console.log(index);
+//     index++;
+// }
+
+/********************************/
+
+// let myArray = ["flash", "batman", "superman"];
+// let arr = 0;
+// while (arr < myArray.length) {
+//     console.log(myArray[arr]);
+//     arr++;
+// }
+
+
+/*************** do-while loop *********************/
+
+// let score = 1;
+
+// do {
+//     console.log(score);
+//     score++;
+// }
+
+/*******************************/
+
+// let score = 11;
+
+// do {
+//     console.log(score);   //11
+//     score++;
+// } while (score <= 10);
